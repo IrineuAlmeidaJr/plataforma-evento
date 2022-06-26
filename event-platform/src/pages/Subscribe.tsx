aa3e21254b8eb09c8157d18bd66357c5f447020e
irineu <irineu.almeida.jr@hotmail.com>
2022-06-26 00:10:14 -0300

    Update Subscribe.tsx

--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -88,7 +88,7 @@ export function Subscribe() {
                 </div>
 
             </div>
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={'/src/assets/code-mockup.png'} className="mt-10" alt={'Code Mockup'} />
         </div>
     )
 }
